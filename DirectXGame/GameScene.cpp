e1c7efe81f0221987c786822b454a3729d28b3eb
ナツリオン <k024g0060@g.neec.ac.jp>
2025-04-16 20:25:41 +0900

    01_03_kadai

--- DirectXGame/GameScene.cpp
+++ DirectXGame/GameScene.cpp
@@ -1,10 +1,31 @@
 #include "GameScene.h"
 
-
 using namespace KamataEngine;
 
-void GameScene::Initialize() {}
+GameScene::~GameScene() { delete model_; }
 
+void GameScene::Initialize() {
+	// ファイル名を指定してテクスチャを読み込む
+	textureHandle_ = TextureManager::Load("IMG_2124.jpg");
+	// 3Dモデルの作成
+	model_ = Model::Create();
+
+	// ワールドトランスフォームの初期化
+	worldTransform_.Initialize();
+	// カメラの初期化
+	camera_.Initialize();
+}
 void GameScene::Update() {}
 
-void GameScene::Draw() {}
+void GameScene::Draw() {
+	// DirectXCommonインスタンスの取得
+	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
+	// 3Dモデル描画前処理
+	Model::PreDraw(dxCommon->GetCommandList());
+
+	// 3Dモデル描画
+	model_->Draw(worldTransform_, camera_, textureHandle_);
+
+	// 3Dモデルの描画後処理
+	Model::PostDraw();
+}
--- DirectXGame/GameScene.h
+++ DirectXGame/GameScene.h
@@ -6,10 +6,31 @@
 class GameScene 
 {
 public:
+	~GameScene();
+	// テクスチャハンドル
+	uint32_t textureHandle_ = 0;
+
+	KamataEngine::Model* model_ = nullptr;
+
+	//ワールドトランスフォーム
+	KamataEngine::WorldTransform worldTransform_;
+	//カメラ
+	KamataEngine::Camera camera_;
+
+
+
+
+
+
+
+
 	//初期化
 	void Initialize();
 	//更新
 	void Update();
 	//描画
 	void Draw();
+
+
+
 };
